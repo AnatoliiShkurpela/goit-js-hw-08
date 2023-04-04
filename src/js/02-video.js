e73dca5560308857173b544633683a53de84d64f
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-04 16:16:18 +0300

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,7 @@
 import Player from '@vimeo/player'; 
 import throttle from 'lodash.throttle'; 
 
+
 const iframe = document.querySelector('#vimeo-player'); 
 const player = new Player(iframe); 
 player.on('timeupdate', throttle(saveCurrentTime, 1000)); 
@@ -17,4 +18,53 @@ function saveCurrentTime() {
       player.setCurrentTime(currentTime); 
     }
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const iframe = document.querySelector('iframe');
+
+// const player = new Player(iframe);
+// const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+// if (getCurrentTime) {
+//   player.setCurrentTime(getCurrentTime);
+// }
+
+// player.on('timeupdate', throttle(currentTime, 1000));
+
+// function currentTime(data) {
+//   let currentTime = data.seconds;
+//   localStorage.setItem('videoplayer-current-time', currentTime);
+// }
+
+
+
+
+
+
+
 
