de12361eb23dfdfcf7a4e176c0e5547dba29d3dd
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-04-06 17:36:34 +0300

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,15 @@
 import Player from '@vimeo/player'; 
 import throttle from 'lodash.throttle'; 
 
-
-const iframe = document.querySelector('#vimeo-player'); 
+const iframe =document.querySelector('#vimeo-player'); 
 const player = new Player(iframe); 
 
-const VIDEO_CURRENT_TIME = 'videoplayer - current - time';
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(function(data) {
   const time = data.seconds;
   localStorage.setItem(VIDEO_CURRENT_TIME, time);
-}, 1000));
+}, ));
 
 const savedTime = localStorage.getItem(VIDEO_CURRENT_TIME);
 if (savedTime) {
